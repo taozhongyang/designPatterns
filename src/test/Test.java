59489d0be7c338c535d46aea3d73702399ed0896
taozy <moniantsin@qq.com>
2018-11-01 09:20:56 +0800

    test

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @program: designPatterns
+ * @description:
+ * @author: taozy
+ * @create: 2018-11-01 09:16
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
